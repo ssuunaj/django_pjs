b98224d9c93f92626461cb6613d9d2242117122c
ssuunaj <jamex027@gmail.com>
2024-02-11 23:58:11 +0100

    django crud

---
+++ crud/food/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Item
+from django.template import loader
+
+# Create your views here.
+
+def index(request):
+    item_list = Item.objects.all()
+    template = loader.get_template('food/index.html')
+    context  = {
+        'item_list':item_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def item(request):
+    return HttpResponse("<h1>This is an item view</h1>")
